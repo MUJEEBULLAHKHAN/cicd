31a69f22b014ef6301d3be4800fac1d0671b02ae
MUJEEB ULLAH KHAN <mukhansahab@gmail.com>
2023-10-02 18:04:54 +0530

    second commit

--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
 # Create your views here.
+def sample(request):
+    return HttpResponse("This is the CI/CD Automation using GITHUB & IIS")
