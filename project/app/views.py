bf60357485892f28586d4d2ba1c32490cd40c054
MUJEEB ULLAH KHAN <mukhansahab@gmail.com>
2023-10-03 12:14:07 +0530

    copy commit2

--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render, HttpResponse
 # Create your views here.
 def sample(request):
-    return HttpResponse("This is the CI/CD Automation using GITHUB & IIS")
+    return HttpResponse("This is the CI/CD Automation using GITHUB & IIS Dynamic")
